2fae9244163407b802e8ffe9fc447ad25ae3c5bf
Erdem Süren <erdem_suren@windowslive.com>
2024-01-31 22:24:07 +0300

    Seperate Projects

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,10 +1,10 @@
-﻿using BulkyWeb.Data;
-using BulkyWeb.Models;
+﻿using Bulky.DataAccess.Data;
+using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
-	public class CategoryController : Controller
+    public class CategoryController : Controller
 	{
 		private readonly ApplicationDbContext _db;
 		public CategoryController(ApplicationDbContext db)
